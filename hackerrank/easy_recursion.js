60fe0afd0e2c580917c2a6910608ec05b332a22d
mussol <gemma.valls@gmail.com>
2017-09-13 17:43:49 +0200

    solve recursion

---
+++ hackerrank/easy_recursion.js
@@ -0,0 +1,34 @@
+// https://www.hackerrank.com/challenges/30-recursion
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function factorial(n) {
+    // Complete this function
+}
+
+function main() {
+    var n = parseInt(readLine());
+    var result = factorial(n);
+    process.stdout.write("" + result + "\n");
+
+}
